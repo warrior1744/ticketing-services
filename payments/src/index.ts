9f02726d0320aa3ff616c09ba5567cc19fbd0c5f
Jim Chang <warrior1744@gmail.com>
2023-02-05 10:50:03 +0800

    added startup message on payment service index

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -5,7 +5,7 @@ import { OrderCancelledListener } from "./events/listeners/order-cancelled-liste
 import { OrderCreatedListener } from "./events/listeners/order-created-listener";
 
 const start = async () => {
-  console.log('Payments Service Starting....')
+  console.log('Payments Service Starting...')
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY is not defined");
   }

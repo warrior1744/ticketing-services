efcafc482cad6313f6ba0a82b277f7493e0bea9a
Jim Chang <warrior1744@gmail.com>
2023-02-05 09:22:52 +0800

    try failed test on the change

--- auth/src/routes/__test__/currentuser.test.ts
+++ auth/src/routes/__test__/currentuser.test.ts
@@ -11,7 +11,7 @@ it('responds info about the current user', async () => {
         .get('/api/users/currentuser')
         .set('Cookie',cookie)
         .send()
-        .expect(200)
+        .expect(400)
 
     expect(response.body.currentUser.email).toEqual('john@example.com')
 })

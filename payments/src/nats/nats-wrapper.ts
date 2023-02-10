c11a7029ddda7db6d03440f8eb9a7ab9c2543929
Jim Chang <warrior1744@gmail.com>
2023-02-10 15:17:12 +0800

    final before depl

--- payments/src/nats/nats-wrapper.ts
+++ payments/src/nats/nats-wrapper.ts
@@ -1,30 +1,32 @@
-import nats, { Stan } from 'node-nats-streaming'
+import nats, { Stan } from "node-nats-streaming";
 
 class NatsWrapper {
-    private _client?: Stan
+  private _client?: Stan;
 
-    get client(){
-        if(!this._client){
-            throw new Error('Connot access NATS client before connecting')
-        }
-        return this._client
+  get client() {
+    if (!this._client) {
+      throw new Error("Connot access NATS client before connecting");
     }
+    return this._client;
+  }
 
-    connect(clusterId: string, clientId: string, url: string){
-        this._client = nats.connect(clusterId, clientId, { url
-        })
-        return new Promise<void>((resolve, reject) => {
-            this._client!.on('connect', () => {
-                console.log('Connected to NATS')
-                resolve()
-            })
+  connect(clusterId: string, clientId: string, url: string) {
+    this._client = nats.connect(clusterId, clientId, {
+      url,
+      waitOnFirstConnect: true,
+    });
+    return new Promise<void>((resolve, reject) => {
+      this._client!.on("connect", () => {
+        console.log("Connected to NATS");
+        resolve();
+      });
 
-            this._client?.on('error', (err) => {
-                console.log(`Error when connecting to NATS: ${err}`)
-                reject(err)
-            })
-        })
-    }
+      this._client?.on("error", (err) => {
+        console.log(`Error when connecting to NATS: ${err}`);
+        reject(err);
+      });
+    });
+  }
 }
 
-export const natsWrapper = new NatsWrapper()
+export const natsWrapper = new NatsWrapper();

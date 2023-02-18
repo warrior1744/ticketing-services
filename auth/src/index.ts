3e6d2aeee5d8620569d9f0b602e1b0e106349f5e
Jim Chang <warrior1744@gmail.com>
2023-02-18 21:24:24 +0800

    auth deploy

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
-  console.log("Auth Service Starting up ...");
+  console.log("Auth Service Starting up .....");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY is not defined");
   }

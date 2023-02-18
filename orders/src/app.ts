d9194a8a0c498a1b4bd7fb23cf105460e385d0f8
Jim Chang <warrior1744@gmail.com>
2023-02-18 20:05:02 +0800

    align the codes, and redeploy all services

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -1,34 +1,34 @@
-import express from 'express'
-import 'express-async-errors'
-import { json } from 'body-parser'
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
 
-import { createOrderRouter } from './routes/new'
-import { showOrderRouter } from './routes/show'
-import { indexOrderRouter } from './routes'
-import { deleteOrderRouter } from './routes/delete'
+import { createOrderRouter } from "./routes/new";
+import { showOrderRouter } from "./routes/show";
+import { indexOrderRouter } from "./routes";
+import { deleteOrderRouter } from "./routes/delete";
 
-import cookieSession from 'cookie-session'
-import { errorHandler, NotFoundError, currentUser } from '@jimtickets/common'
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError, currentUser } from "@jimtickets/common";
 
-const app = express()
-app.set('trust proxy', true) //traffic proxied through ingress nginx
-app.use(json())
+const app = express();
+app.set("trust proxy", true); //traffic proxied through ingress nginx
+app.use(json());
 app.use(
-    cookieSession({
-        signed: false, //disable encryption (commonly used)
-        secure: false, //process.env.NODE_ENV !== 'test' //use https connection
-    })
-)
-app.use(currentUser)
-app.use(createOrderRouter)
-app.use(showOrderRouter)
-app.use(indexOrderRouter)
-app.use(deleteOrderRouter)
+  cookieSession({
+    signed: false, //disable encryption (commonly used)
+    secure: false, //process.env.NODE_ENV !== 'test' //use https connection
+  })
+);
+app.use(currentUser);
+app.use(createOrderRouter);
+app.use(showOrderRouter);
+app.use(indexOrderRouter);
+app.use(deleteOrderRouter);
 
-app.get('*', async (req, res) => {
-    throw new NotFoundError()
-})
+app.get("*", async (req, res) => {
+  throw new NotFoundError();
+});
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-export { app }
+export { app };
--- payments/src/app.ts
+++ payments/src/app.ts
@@ -1,27 +1,27 @@
-import express from 'express'
-import 'express-async-errors'
-import { json } from 'body-parser'
-import cookieSession from 'cookie-session'
-import { errorHandler, NotFoundError, currentUser } from '@jimtickets/common'
-import { createChargeRouter } from './routes/new'
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError, currentUser } from "@jimtickets/common";
+import { createChargeRouter } from "./routes/new";
 
-const app = express()
-app.set('trust proxy', true) //traffic proxied through ingress nginx
-app.use(json())
+const app = express();
+app.set("trust proxy", true); //traffic proxied through ingress nginx
+app.use(json());
 app.use(
-    cookieSession({
-        signed: false, //disable encryption (commonly used)
-        secure: false, //process.env.NODE_ENV !== 'test' //use https connection
-    })
-)
+  cookieSession({
+    signed: false, //disable encryption (commonly used)
+    secure: false, //process.env.NODE_ENV !== 'test' //use https connection
+  })
+);
 
-app.use(currentUser)
-app.use(createChargeRouter)
+app.use(currentUser);
+app.use(createChargeRouter);
 
-app.get('*', async (req, res) => {
-    throw new NotFoundError()
-})
+app.get("*", async (req, res) => {
+  throw new NotFoundError();
+});
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-export { app }
+export { app };
